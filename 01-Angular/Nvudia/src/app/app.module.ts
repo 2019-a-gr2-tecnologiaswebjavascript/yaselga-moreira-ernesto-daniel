f856dc3f14d61f0e731c833759ae7ddee064559b
Ernesto Yaselga <ernestoyaselga@iMac-de-USRDEL-103.local>
2019-05-06 17:16:43 -0500

    errores corregidos, revisar video de clase 0 6/05/19

--- 01-Angular/Nvudia/src/app/app.module.ts
+++ 01-Angular/Nvudia/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { CarruselComponent } from './carrusel/carrusel.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { SeccionComponent } from './seccion/seccion.component';
+import { SeccionItemComponent } from './seccion-item/seccion-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarruselComponent,
-    NavbarComponent
+    NavbarComponent,
+    SeccionComponent,
+    SeccionItemComponent
   ],
   imports: [
     BrowserModule,
