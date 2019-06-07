f7064aa43169da179fea0a104e4a2631f4c6df98
Ernesto Yaselga <ernestoyaselga@iMac-de-USRDEL-103.local>
2019-06-07 18:02:45 -0500

    Sails

---
+++ 05-backend/tienda-backend/api/models/Usuario.ts
@@ -0,0 +1,22 @@
+/**
+ * Usuario.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    nombre:{
+      type:'string'
+    },
+    apellido:{
+      type:'string',
+      required: true
+    }
+
+  },
+  
+};
+
